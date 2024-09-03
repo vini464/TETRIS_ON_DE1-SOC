1695dfa1b2c7c48cbef6ac62d13aef48ff73b347
Pedro Henrique <phasilva2002@outlook.com>
2024-09-03 15:58:48 -0300

    atualização memoria

--- g-sensor.c
+++ g-sensor.c
@@ -5,5 +5,5 @@
 #include <fcntl.h>
 
 #define ACCEL_REG_SIZE  0x39  // 57 em hexadecimal
-#define ACCEL_WRITE_ADDR 0xA7 //0xA7 = Endereço de Escrita do I2C
-#define ACCEL_READ_ADDR 0xA6  //0xA6 = Endereço de Leitura do I2C
+#define ACCEL_WRITE_ADDR 0xA7 //0xA7 = Offset de Leitura do I2C
+#define ACCEL_READ_ADDR 0xA6  //0xA6 = Offset de Escrita do I2C
