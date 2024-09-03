fe092145c3cb95fd9510369472951552c4e41af8
Pedro Henrique <phasilva2002@outlook.com>
2024-09-03 01:47:00 -0300

    Endereço de Memoria
    Achei os valores da memoria, seja para o I2C, seja o tamanho em bytes para se mapear.
    Estou entendo as possibilidades, aparentemente existe a função mmap (que é o motivo de ter o REG_SIZE) onde se mapeia a memoria, e tem a possibilidade de fazer via I2C, porém, não sei qual é o certo

---
+++ g-sensor.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#define ACCEL_REG_SIZE  0x39  // 57 em hexadecimal
+#define ACCEL_WRITE_ADDR 0xA7 //0xA7 = Endereço de Escrita do I2C
+#define ACCEL_READ_ADDR 0xA6  //0xA6 = Endereço de Leitura do I2C
